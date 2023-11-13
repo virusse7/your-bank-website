0e77bc5efcd9d7fe4998bf2b99788c37d3364a9d
Daniel Wieczorek <danielwieczorek901@gmail.com>
2023-11-13 14:52:32 +0100

    created styles to login form

--- src/features/YourBank/Login/LoginForm/index.js
+++ src/features/YourBank/Login/LoginForm/index.js
@@ -1,25 +1,32 @@
 import React from 'react'
-import { Header, Wrapper } from './styled'
+import { Header, Wrapper, Title, Box, ActionContainer, LoginButton, SignUpButton, Line, Text, ForgotPasswordButton } from './styled'
 
 const LoginForm = () => {
     return (
         <Wrapper>
-            <Header>Login</Header>
-            <p>Welcome back! Please log in to access your account.</p>
-            <div>
-                <input type='email' placeholder='Enter your Email' />
-                <input type='password' placeholder='Enter your Password' />
-            </div>
-            <div>Forgot Password?</div>
-            <div>
-                <a>Login</a>
-                <a>Sign Up</a>
-            </div>
-            <div>
-                <img />
-                <img />
-                <img />
-            </div>
+            <Box>
+                <Header>Login</Header>
+                <Title>Welcome back! Please log in to access your account.</Title>
+                <div>
+                    <input type='email' placeholder='Enter your Email' />
+                    <input type='password' placeholder='Enter your Password' />
+                </div>
+                <ForgotPasswordButton>Forgot Password?</ForgotPasswordButton>
+                <ActionContainer>
+                    <LoginButton>Login</LoginButton>
+                    <SignUpButton>Sign Up</SignUpButton>
+                    <Text>
+                        <Line />
+                        Or Continue with
+                        <Line />
+                    </Text>
+                    <div>
+                        <img />
+                        <img />
+                        <img />
+                    </div>
+                </ActionContainer>
+            </Box>
         </Wrapper>
     )
 }
--- src/features/YourBank/Login/LoginForm/styled.js
+++ src/features/YourBank/Login/LoginForm/styled.js
@@ -5,4 +5,52 @@ export const Wrapper = styled.div`
     text-align: center;
 `;
 
-export const Header = styled.h2``;
+export const Header = styled.h2`
+    color: ${({ theme }) => theme.green60};
+    font-size: 48px;
+    margin: 0 0 20px 0;
+`;
+
+export const Title = styled.div`
+    color: ${({ theme }) => theme.grey75};
+    margin-bottom: 80px;
+`;
+
+export const Box = styled.div`
+    padding: 100px;
+`;
+
+export const ActionContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 24px;
+    padding: 0 240px;
+`;
+
+export const LoginButton = styled.button`
+    padding: 18px 20px;
+    border-radius: 63px;
+    border: none;
+    background-color: ${({ theme }) => theme.green60};
+`;
+
+export const SignUpButton = styled(LoginButton)`
+    background-color: ${({ theme }) => theme.grey20};
+    color: ${({ theme }) => theme.white};
+`;
+
+export const Line = styled.div`
+    border-top: 1px solid ${({ theme }) => theme.grey70};
+`;
+
+export const Text = styled.div`
+    color: ${({ theme }) => theme.grey70};
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    justify-content: center;
+    align-items: center;
+`;
+
+export const ForgotPasswordButton = styled.a`
+
+`;
