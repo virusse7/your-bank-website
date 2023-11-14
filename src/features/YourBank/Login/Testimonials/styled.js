976df3f0406265a5902620d9ac395c5368ece86e
Daniel Wieczorek <danielwieczorek901@gmail.com>
2023-11-14 16:43:19 +0100

    added basic styles for testimonials

---
+++ src/features/YourBank/Login/Testimonials/styled.js
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+
+`;
+
+export const Header = styled.h2`
+    font-size: 48px;
+`;
+
+export const TextGreen = styled.span`
+    color: ${({ theme }) => theme.green60};
+`;
+
+export const SubTitle = styled.p`
+    color: ${({ theme }) => theme.grey70};
+`;
+
+export const Tile = styled.div`
+    
+`;
