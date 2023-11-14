b8ff6d310ed87bf331fa3d99935e4fc041069bdf
Daniel Wieczorek <danielwieczorek901@gmail.com>
2023-11-14 16:44:09 +0100

    basic structure of testimonials

--- src/features/YourBank/Login/Testimonials/index.js
+++ src/features/YourBank/Login/Testimonials/index.js
@@ -1,8 +1,33 @@
+import {
+    Wrapper,
+    Header,
+    TextGreen,
+    SubTitle,
+    Tile
+} from './styled';
 import React from 'react'
 
 const Testimonials = () => {
     return (
-        <div>Testimonials</div>
+        <Wrapper>
+            <div>
+                <div>
+                    <Header>Our <TextGreen>Testimonials</TextGreen></Header>
+                    <SubTitle>
+                        Discover how YourBank has transformed lives with innovative digital solutions and personalized customer service. See why our clients trust us for a secure and prosperous financial journey
+                    </SubTitle>
+                    <div>
+                        <button>For Individuals</button>
+                        <button>For Businesses</button>
+                    </div>
+                </div>
+            </div>
+            <Tile>
+                {/* ikona */}
+                {/* text */}
+                {/* imie */}
+            </Tile>
+        </Wrapper>
     )
 }
 
