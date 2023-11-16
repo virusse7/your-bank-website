4fbe6b9cfc29346d87419e823ab1030b7974302e
Daniel Wieczorek <danielwieczorek901@gmail.com>
2023-11-16 11:36:31 +0100

    built basic component of testimonials

--- src/features/YourBank/common/Testimonials/index.js
+++ src/features/YourBank/common/Testimonials/index.js
@@ -3,28 +3,40 @@ import {
     Header,
     TextGreen,
     SubTitle,
-    Tile
+    Tile,
+    Container,
+    Button,
+    Buttons,
+    MainContainer
 } from "./styled";
+import { testimonialsData } from "./testimonialsData";
+import doubleQuotes from "./doubleQuotes.svg"
+
 
 const Testimonials = () => {
     return (
         <Wrapper>
-            <div>
+            <MainContainer>
                 <div>
                     <Header>Our <TextGreen>Testimonials</TextGreen></Header>
                     <SubTitle>
                         Discover how YourBank has transformed lives with innovative digital solutions and personalized customer service. See why our clients trust us for a secure and prosperous financial journey
                     </SubTitle>
-                    <div>
-                        <button>For Individuals</button>
-                        <button>For Businesses</button>
-                    </div>
                 </div>
-            </div>
+                <Buttons>
+                    <Button>For Individuals</Button>
+                    <Button>For Businesses</Button>
+                </Buttons>
+            </MainContainer>
             <Tile>
-                {/* ikona */}
-                {/* text */}
-                {/* imie */}
+                {testimonialsData.map((testimonial) => (
+                    <Container key={testimonial.name}>
+                        <img src={doubleQuotes} width="60px" alt="double quotes" />
+                        <p>{testimonial.text}</p>
+                        <TextGreen>{testimonial.name}</TextGreen>
+                    </Container>
+                ))}
+
             </Tile>
         </Wrapper>
     );
--- src/features/YourBank/common/Testimonials/styled.js
+++ src/features/YourBank/common/Testimonials/styled.js
@@ -17,5 +17,41 @@ export const SubTitle = styled.p`
 `;
 
 export const Tile = styled.div`
-    
+    display: flex;
+`;
+
+export const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 50px;
+    margin: 0 80px 0 80px;
+`;
+
+export const Button = styled.button`
+    padding: 14px 24px;
+    border-radius: 140px;
+    background: none;
+    border: none;
+    color: ${({ theme }) => theme.white};
+
+    &:active {
+        background: ${({ theme }) => theme.green60};
+        color: ${({ theme }) => theme.black};
+    }
+`;
+
+export const Buttons = styled.div`
+    padding: 14px;
+    background: ${({ theme }) => theme.grey11};
+    border: 1px solid ${({ theme }) => theme.grey15};
+    border-radius: 82px;
+    display: flex;
+`;
+
+export const MainContainer = styled.div`
+    display: flex;
+    align-items: flex-end;
+    gap: 300px;
+    margin-bottom: 100px;
 `;
